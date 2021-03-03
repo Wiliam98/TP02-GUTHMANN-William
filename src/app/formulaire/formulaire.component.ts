9327707d18e462539b0e8239b66368e78ee1d979
wguthmann <williamguthmann@gmail.com>
2021-03-03 11:02:38 +0100

    form correction

--- src/app/formulaire/formulaire.component.ts
+++ src/app/formulaire/formulaire.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormulaireComponent implements OnInit {
 
+  public valid1 : boolean = false;
+
   constructor() { }
 
   click() {
